3045ba2f4bd4f1041bc37ae3fbfb999017c377d6
vpersie9 <834538028@qq.com>
2015-12-23 14:02:23 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cottage
+# The file called spider.py is a test for my learning, and it helped me to grab some jokes I want from a website.
---
+++ tiny_spider.py
@@ -0,0 +1,28 @@
+__author__ = 'vpersie9'
+# -*-coding:utf-8-*-
+import re
+import requests
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+class recode():
+    def scrapy_code(self,url,headers):
+        html=requests.get(url,headers=headers).text
+        code=re.findall('div class="content">(.*?)<',html,re.S)
+        code_we_need=[]
+        for each in code:
+            code_we_need.append(each)
+        return code_we_need
+
+    def save_code(self,code_print):
+        f=open('code.txt','a')
+        for each in code_print:
+            f.writelines(each)
+        f.close()
+
+if __name__=="__main__":
+    url="http://www.qiushibaike.com/"
+    headers={"User-Agent":"Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36"}
+    get_code=recode()
+    code_show=get_code.scrapy_code(url,headers)
+    get_code.save_code(code_show)
